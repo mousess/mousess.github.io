3d5cd417f86f5e9311b52845a8dc366e5cde9b5d
mousess <38389205+mousess@users.noreply.github.com>
2019-08-31 10:00:10 +0700

    Create menuku.js

---
+++ menuku.js
@@ -0,0 +1,17 @@
+
+function ShowBox() {
+    if (confirm("Anda Akan Dialihkan Ke Menu Facebook !!!") == true) {
+        location.href = "https://www.facebook.com";
+        return true;
+    } else {
+        return false;
+    }
+}
+function ShowBox2() {
+    if (confirm("Anda Akan Dialihkan Ke Menu Utama Dicoding !!!") == true) {
+        location.href = "https://www.dicoding.com";
+        return true;
+    } else {
+        return false;
+    }
+}
